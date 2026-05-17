2d7bbac624a23e3db4ad727de550334b7ccdaa62
He3rmit <mateo2904rex@proton.me>
2026-05-18 02:06:20 +0800

    fix an aggressive flag hogging cpu power and temp with higher resolution wallpapers

--- hyprland/scripts/wallpaper-selector.sh
+++ hyprland/scripts/wallpaper-selector.sh
@@ -41,8 +41,9 @@ apply_wallpaper() {
             fi
             
             # Using both loop-file and loop-playlist, plus keep-open to prevent premature exiting
-            mpvpaper -f "$monitor" "$wall" \
-                -o "--no-audio --loop-file=inf --loop-playlist=inf --keep-open=yes --fullscreen --panscan=1.0 --no-osc --no-osd-bar \
+            # Added -p to pause when wallpaper is hidden and --hwdec=auto for hardware GPU decoding
+            mpvpaper -p -f "$monitor" "$wall" \
+                -o "--hwdec=auto --no-audio --loop-file=inf --loop-playlist=inf --keep-open=yes --fullscreen --panscan=1.0 --no-osc --no-osd-bar \
                     --geometry=100%x100% $fade_opt"
         else
             # Static image settings (swaybg is lighter and more stable for images)
